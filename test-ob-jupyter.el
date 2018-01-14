9c4e0eda15d0e7b8bbd482c170f1348d59acd5d5
Trevor Murphy <trevor.m.murphy@gmail.com>
2018-01-14 13:10:28 -0800

    Define an ERT deftest-parametrize macro.
    This is inspired by pytest’s mark.parametrize decorator.  The goal is to
    run one test body over many varying input parameters.

--- test-ob-jupyter.el
+++ test-ob-jupyter.el
@@ -7,6 +7,23 @@
 (require 'ert)
 (require 'ob-jupyter)
 
+(defmacro ert-deftest-parametrize (prefix params values &rest body)
+  "Create ERT deftests from a list of parameters.
+
+Give them names starting with PREFIX, e.g. PREFIX-0, PREFIX-1, etc.
+Bind PARAMS to sequential elements from VALUES and execute test BODY."
+  (declare (indent defun))
+  (cl-loop for i below (length values)
+           collect
+           `(ert-deftest ,(intern
+                           (concat
+                            (symbol-name prefix) "-" (number-to-string i)))
+                ()
+              (cl-destructuring-bind ,params (list ,@(nth i values))
+                ,@body))
+           into result
+           finally return (cons 'progn result)))
+
 (ert-deftest ob-jupyter-support-zmq ()
   "Does ob-jupyter know enough about 0MQ?"
   (should (fboundp 'zmq))
